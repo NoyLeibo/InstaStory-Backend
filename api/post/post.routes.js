25e534cc0f38618b41389902a2b25f0b514f93b4
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-03-20 21:13:26 +0200

    update build

--- api/post/post.routes.js
+++ api/post/post.routes.js
@@ -3,6 +3,7 @@ import express from 'express'
 import { requireAuth, requireAdmin } from '../../middlewares/requireAuth.middleware.js'
 import { log } from '../../middlewares/logger.middleware.js'
 import { getPosts, getPostById, addPost, updatePost, removePost, addPostMsg, removePostMsg } from './post.controller.js'
+import { logger } from '../../services/logger.service.js'
 
 export const postRoutes = express.Router()
 
